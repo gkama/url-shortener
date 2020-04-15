2d84d541382e2e5b1e10e1e1c8466698f3f796d8
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-15 11:32:37 -0400

    update url repository

--- url.shortener.services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace url.shortener.services
-{
-    public class Class1
-    {
-    }
-}
---
+++ url.shortener.services/UrlRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+namespace url.shortener.services
+{
+    public class UrlRepository
+    {
+        private readonly ILogger<UrlRepository> _logger;
+
+        public UrlRepository(ILogger<UrlRepository> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
