33d8cd13c06bab776e1ea61b9d7f09c734f720e9
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-15 11:32:42 -0400

    update gkamaurl

---
+++ url.shortener.data/GkamaUrl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace url.shortener.data
+{
+    public class GkamaUrl
+    {
+        public string Id { get; set; }
+        public Guid PublicKey { get; set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string Target { get; set; }
+        public string ShortUrl { get; set; }
+    }
+}
