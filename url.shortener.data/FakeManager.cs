c04ce32b3c2b74c51dae88c1fdf44597b8283f83
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-15 13:10:50 -0400

    add fake data manager

---
+++ url.shortener.data/FakeManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace url.shortener.data
+{
+    public class FakeManager
+    {
+        private readonly UrlContext _context;
+
+        public FakeManager(UrlContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task UseFakeContextAsync()
+        {
+            await _context.Urls
+                .AddRangeAsync(GetFakeGkamaUrls());
+        }
+
+        public IEnumerable<GkamaUrl> GetFakeGkamaUrls()
+        {
+            return new List<GkamaUrl>()
+            {
+                new GkamaUrl()
+                {
+                    Target = "https://google.com",
+                    ShortUrl = "https://gkama.it/g"
+                },
+                new GkamaUrl()
+                {
+                    Target = "https://google.com",
+                    ShortUrl = "https://gkama.it/google"
+                }
+            };
+        }
+    }
+}
