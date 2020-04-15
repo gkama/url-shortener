672ab5152ad5ab35c3cd3d94a196fbda48dafb54
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-15 12:45:46 -0400

    add interfaces and db context

--- url.shortener.data/GkamaUrl.cs
+++ url.shortener.data/GkamaUrl.cs
@@ -2,9 +2,9 @@
 
 namespace url.shortener.data
 {
-    public class GkamaUrl
+    public class GkamaUrl : IPublicKeyId, IGkamaUrl
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public Guid PublicKey { get; set; } = Guid.NewGuid();
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public string Target { get; set; }
---
+++ url.shortener.data/UrlContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace url.shortener.data
+{
+    public class UrlContext : DbContext
+    {
+        public virtual DbSet<GkamaUrl> Urls { get; set; }
+
+        public UrlContext(DbContextOptions<UrlContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GkamaUrl>(e =>
+            {
+                e.ToTable("gkama_url");
+
+                e.HasKey(x => x.Id);
+            });
+        }
+    }
+}
