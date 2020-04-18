9293551f0029ccfb66a151c3d9a0ebdc1afe6419
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-18 17:28:49 -0400

    add url exception

---
+++ url.shortener.data/UrlException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace url.shortener.data
+{
+    public class UrlException : ApplicationException
+    {
+        public int StatusCode { get; set; }
+        public string ContentType { get; set; }
+
+        public UrlException(int StatusCode)
+        {
+            this.StatusCode = StatusCode;
+        }
+
+        public UrlException(string Message) : base(Message)
+        {
+            this.StatusCode = (int)HttpStatusCode.InternalServerError;
+        }
+
+        public UrlException(int StatusCode, string Message) : base(Message)
+        {
+            this.StatusCode = StatusCode;
+        }
+
+        public UrlException(HttpStatusCode StatusCode, string Message) : base(Message)
+        {
+            this.StatusCode = (int)StatusCode;
+        }
+
+        public UrlException(int StatusCode, Exception Inner) : this(StatusCode, Inner.ToString()) { }
+        public UrlException(HttpStatusCode StatusCode, Exception Inner) : this(StatusCode, Inner.ToString()) { }
+        public UrlException(int StatusCode, JsonElement ErrorObject) : this(StatusCode, ErrorObject.ToString()) { this.ContentType = @"application/problem+json"; }
+    }
+}
