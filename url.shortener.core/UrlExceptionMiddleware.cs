28f6a544244302ea47d148673c857fecc7b8631d
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-18 17:34:36 -0400

    add url exception middleware

---
+++ url.shortener.core/UrlExceptionMiddleware.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+
+using url.shortener.data;
+
+namespace url.shortener.core
+{
+    public class UrlExceptionMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+        private readonly ILogger _log;
+        private readonly IWebHostEnvironment _env;
+
+        private const string _defaultMessage = "An unexpected error has occurred.";
+
+        public UrlExceptionMiddleware(RequestDelegate next, ILogger<UrlExceptionMiddleware> log, IWebHostEnvironment env)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+            _log = log ?? throw new ArgumentNullException(nameof(log));
+            _env = env ?? throw new ArgumentNullException(nameof(env));
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception e)
+            {
+                if (httpContext.Response.HasStarted)
+                {
+                    _log.LogWarning("The response has already started, the http status code middleware will not be executed.");
+                    throw;
+                }
+
+                var id = string.IsNullOrEmpty(httpContext?.TraceIdentifier)
+                    ? Guid.NewGuid().ToString()
+                    : httpContext.TraceIdentifier;
+
+                _log.LogError(e, $"an exception was thrown during the request. {id}");
+
+                await WriteExceptionResponseAsync(httpContext, e, id);
+            }
+        }
+
+        private async Task WriteExceptionResponseAsync(HttpContext httpContext, Exception e, string id)
+        {
+            var canViewSensitiveInfo = _env
+                .IsDevelopment();
+
+            var problem = new ProblemDetails()
+            {
+                Title = canViewSensitiveInfo
+                    ? e.Message
+                    : _defaultMessage,
+                Detail = canViewSensitiveInfo
+                    ? e.Demystify().ToString()
+                    : null,
+                Instance = $"nlp:error:{id}"
+            };
+
+            if (e is UrlException ue)
+                problem.Status = ue.StatusCode;
+            else
+                problem.Status = StatusCodes.Status500InternalServerError;
+
+            var problemjson = JsonSerializer
+                .Serialize(problem);
+
+            httpContext.Response.StatusCode = problem.Status ?? StatusCodes.Status500InternalServerError;
+            httpContext.Response.ContentType = "application/problem+json";
+
+            await httpContext.Response
+                .WriteAsync(problemjson);
+        }
+    }
+
+    public static class HttpStatusCodeExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseUrlException(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<UrlExceptionMiddleware>();
+        }
+    }
+}
