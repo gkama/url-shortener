4ac55c23f91fe15b424d6fc6fd90c162369a660c
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-21 00:00:37 -0400

    add more fake data

--- url.shortener.data/FakeManager.cs
+++ url.shortener.data/FakeManager.cs
@@ -30,15 +30,23 @@ namespace url.shortener.data
             {
                 new GkamaUrl()
                 {
+                    Id = 1,
                     PublicKey = new Guid("a733b9f0-d716-40b4-921b-aef79b2f1a04"),
                     Target = "https://www.google.com/maps/place/London,+UK/@51.5287718,-0.2416793,11z/data=!3m1!4b1!4m5!3m4!1s0x47d8a00baf21de75:0x52963a5addd52a99!8m2!3d51.5073509!4d-0.1277583",
                     ShortUrl = "https://gkama.it/london"
                 },
                 new GkamaUrl()
                 {
+                    Id = 2,
                     PublicKey = new Guid("2f340700-a698-4644-a6a2-069f73b92ad4"),
                     Target = "https://www.google.com/maps/place/Sydney+NSW,+Australia/@-33.8478796,150.7918936,11z/data=!3m1!4b1!4m5!3m4!1s0x6b129838f39a743f:0x3017d681632a850!8m2!3d-33.8688197!4d151.2092955",
                     ShortUrl = "https://gkama.it/sydney"
+                },
+                new GkamaUrl()
+                {
+                    Id = 3,
+                    PublicKey = new Guid("f7578931-6b73-4606-98ed-4dbfe5e2b0b8"),
+                    Target = "https://www.google.com/maps/place/Sofia,+Bulgaria/@42.695537,23.2539074,12z/data=!3m1!4b1!4m5!3m4!1s0x40aa8682cb317bf5:0x400a01269bf5e60!8m2!3d42.6977082!4d23.3218675"
                 }
             };
         }
