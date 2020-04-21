0db11c652582c84cac977ed2e500e7f10ff64764
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-21 10:59:35 -0400

    add fake data

--- url.shortener.data/FakeManager.cs
+++ url.shortener.data/FakeManager.cs
@@ -53,6 +53,13 @@ namespace url.shortener.data
                     Id = 4,
                     PublicKey = new Guid("e6866071-382d-4176-864a-1e7b1aa73b67"),
                     Target = "https://www.google.com/maps/place/Berlin,+Germany/@52.5069704,13.2846512,11z/data=!3m1!4b1!4m5!3m4!1s0x47a84e373f035901:0x42120465b5e3b70!8m2!3d52.5200066!4d13.404954"
+                },
+                new GkamaUrl()
+                {
+                    Id = 5,
+                    PublicKey = new Guid("ec3171ff-b029-4b73-8994-005a3712d54b"),
+                    Target = "https://www.google.com/maps",
+                    ShortUrl = "https://gkama.it/5"
                 }
             };
         }
