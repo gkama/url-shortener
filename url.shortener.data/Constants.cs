c5cdf09f7502ac95d1ef668385bb96908a642ff2
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-22 10:26:44 -0400

    add constants

---
+++ url.shortener.data/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace url.shortener.data
+{
+    public static class Constants
+    {
+        public static string AlphaNumeric => "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        public static int AlphaNumericLength => AlphaNumeric.Length;
+
+        public static Random Random => new Random();
+    }
+}
