cdb5a18aa76c0d48852f4f6def31d1e0c08a832a
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-22 10:28:20 -0400

    update to remove random

--- url.shortener.data/Constants.cs
+++ url.shortener.data/Constants.cs
@@ -8,7 +8,5 @@ namespace url.shortener.data
     {
         public static string AlphaNumeric => "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         public static int AlphaNumericLength => AlphaNumeric.Length;
-
-        public static Random Random => new Random();
     }
 }
