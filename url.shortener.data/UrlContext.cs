c6ec927ce2655b117f0eb25b1bf837e592b83b44
Georgi Kamacharov <gkamacharov@gmail.com>
2020-04-29 00:25:00 -0400

    add gkama url metadata to context

--- url.shortener.data/UrlContext.cs
+++ url.shortener.data/UrlContext.cs
@@ -9,6 +9,7 @@ namespace url.shortener.data
     public class UrlContext : DbContext
     {
         public virtual DbSet<GkamaUrl> Urls { get; set; }
+        public virtual DbSet<GkamaUrlMetadata> UrlMetadata { get; set; }
  
         public UrlContext(DbContextOptions<UrlContext> options)
             : base(options)
@@ -30,6 +31,23 @@ namespace url.shortener.data
                 e.Property(x => x.CreatedAt).HasColumnName("created_at").HasColumnType("datetime").IsRequired();
                 e.Property(x => x.Target).HasColumnName("target").HasMaxLength(1000).IsRequired();
                 e.Property(x => x.ShortUrl).HasColumnName("short_url").HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<GkamaUrlMetadata>(e =>
+            {
+                e.ToTable("gkama_url_metadata");
+
+                e.HasKey(x => x.Target);
+
+                e.Property(x => x.Target).HasColumnName("target").HasMaxLength(1000).IsRequired();
+                e.Property(x => x.PublicKey).HasColumnName("public_key").IsRequired();
+                e.Property(x => x.CreatedAt).HasColumnName("created_at").HasColumnType("datetime").IsRequired();
+                e.Property(x => x.Scheme).HasColumnName("scheme").HasMaxLength(100);
+                e.Property(x => x.Domain).HasColumnName("domain").HasMaxLength(100);
+                e.Property(x => x.Port).HasColumnName("port");
+                e.Property(x => x.Path).HasColumnName("path").HasMaxLength(100);
+                e.Property(x => x.Query).HasColumnName("query");
+                e.Property(x => x.Fragment).HasColumnName("fragment");
             });
         }
     }
